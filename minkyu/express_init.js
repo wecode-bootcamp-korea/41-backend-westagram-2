2ae4ce85e2dc64fdd23a1173707dd94bd4fbd47c
minkyu_lee <0106995@naver.com>
2022-12-22 10:52:29 +0900

    Modify: 코드 내 오류 수정

--- minkyu/express_init.js
+++ minkyu/express_init.js
@@ -1,9 +1,15 @@
-const http = require('http');
+// env variables
+
+require("dotenv").config();
+
+// built-in package
+
+
+// third party packages
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+
 
 const { DataSource } = require('typeorm');
 
@@ -21,21 +27,20 @@ myDataSource.initialize()
         console.log("Data Source has been initialized!")
     });
 
-app = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan(''));
+app.use(morgan('tiny'));
 
 app.get("/ping", (req,res) =>{
     res.json({message : "pong"});
 });
 
-const server = http.createServer(app);
 const PORT = process.env.PORT;
 
 const start = async () => {
-    server.listen(PORT, () => console.log(`server is listenning on ${PORT}`));
+    app.listen(PORT, () => console.log(`server is listenning on ${PORT}`));
 };
 
 start()
