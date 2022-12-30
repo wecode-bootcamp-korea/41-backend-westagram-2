75d403aaef410c75aeed99bac1ebc7c1ce04b5b5
Kwang-Hwi Kim <kkh940227@gmail.com>
2022-12-31 01:09:25 +0900

    MODIFY : app.js 수정

--- kwanghwi/myapp/app.js
+++ kwanghwi/myapp/app.js
@@ -2,11 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./routes");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { validateToken } = require("./middlewares/auth");
-
-const userRoutes = require("./routes/userRouter");
 
 const dotenv = require("dotenv");
 dotenv.config();
